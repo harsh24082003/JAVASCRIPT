83ee33011da3afb2f0ecac606e37068c2e5988f0
harsh24082003 <hs3935873@gmail.com>
2025-04-20 22:42:47 +0530

    Added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=13313
+let accountEmail="hs@gmail.com"
+var accountPassword="1234"
+accountCity="Delhi"
+let accountState;
+
+// accountId=23; // cannot be reassigned
+
+accountEmail="h1@gmail.com"
+accountPassword="12345"
+accountCity="Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
