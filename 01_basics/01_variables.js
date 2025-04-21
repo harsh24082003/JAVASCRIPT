9ce041b7a857d2035cb67b4d4d6f5ddafa6d3bb6
harsh24082003 <hs3935873@gmail.com>
2025-04-21 16:15:02 +0530

    added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,10 @@ var accountPassword="1234"
 accountCity="Delhi"
 let accountState;
 
+/*
+prefer not use var because of issue in block scope and functional scope
+*/
+
 // accountId=23; // cannot be reassigned
 
 accountEmail="h1@gmail.com"
