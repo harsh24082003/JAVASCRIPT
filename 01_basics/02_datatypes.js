7c808159aa6342d9f7570d588313edb301cbc8f3
harsh24082003 <hs3935873@gmail.com>
2025-04-21 16:15:58 +0530

    added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let name="Harsh"
+let age=22
+let isLoggedIn=true
+let state //undefined when no value is assigned
+
+console.log(typeof name) //string
+console.log(typeof age) //number
+console.log(typeof isLoggedIn) //boolean
+console.log(typeof null) //object (this is a bug in JS)
+console.log(typeof undefined) //undefined
