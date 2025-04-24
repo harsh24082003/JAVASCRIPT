ef1dc398ecef83f3138213848783d310f2bbbc7a
harsh24082003 <hs3935873@gmail.com>
2025-04-25 01:12:35 +0530

    Added

---
+++ 01_basics/07_NumsAndMaths.js
@@ -0,0 +1,31 @@
+const score=400
+
+const balance=new Number(1000.22333)
+// console.log(score) // 400
+// console.log(balance) // [Number: 1000.22333]
+// console.log(balance.toString()) // 1000.22333
+// console.log(balance.toString(3)) // binary
+// console.log(score.toString()) // 400
+
+// console.log(balance.toFixed(2))  // 1000.22
+
+const otherNumber=1123.6
+// console.log(otherNumber.toPrecision(4)) // 1123.6
+
+// console.log(Number.MAX_VALUE) // 1.7976931348623157e+308
+// console.log(Number.MIN_VALUE) // 5e-324
+
+
+
+// +++++++++++++++++ Math ++++++++++++++++++
+
+console.log(Math.PI) // 3.141592653589793
+console.log(Math.abs(-10)) // 10
+console.log(Math.ceil(4.3)) // 5
+console.log(Math.floor(4.3)) // 4
+console.log(Math.round(4.3)) // 4
+console.log(Math.min(4, 5, 6, 7, 8)) // 4
+console.log(Math.max(4, 5, 6, 7, 8)) // 8
+
+console.log(Math.random()) // 0.123456789
+console.log((Math.random()*10)+1) // 1-10
